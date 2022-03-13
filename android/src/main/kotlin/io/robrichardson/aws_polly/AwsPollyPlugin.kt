489cdd85bf724a53e5a6893838ad67186b05f529
Rob Richardson <rob@robrichardson.io>
2022-03-12 16:18:42 -0800

    Building

--- android/src/main/kotlin/io/robrichardson/aws_polly/AwsPollyPlugin.kt
+++ android/src/main/kotlin/io/robrichardson/aws_polly/AwsPollyPlugin.kt
@@ -10,26 +10,33 @@ import io.flutter.plugin.common.MethodChannel.Result
 
 /** AwsPollyPlugin */
 class AwsPollyPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
-
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "aws_polly")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    class MethodCalls {
+        companion object {
+            const val initialize = "initialize"
+            const val getUrl = "getUrl"
+        }
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel : MethodChannel
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "aws_polly")
+        channel.setMethodCallHandler(this)
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        // if (call.method == "getPlatformVersion") {
+        //   result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        // } else {
+        result.notImplemented()
+        // }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
